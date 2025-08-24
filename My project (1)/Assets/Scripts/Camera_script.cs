623527bc39e7809448d9473d774d9f1dbc8c24ed
EmmaTheCattt <Emmabuhl2004@gmail.com>
2025-08-24 20:45:00 +0200

    Maybe soon work....

--- My project (1)/Assets/Scripts/Camera_script.cs
+++ My project (1)/Assets/Scripts/Camera_script.cs
@@ -2,37 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Camera_script : MonoBehaviour
 {
     public Transform[] Cam_pos;
-    public GameObject player;
+    public Transform player;
 
-    public Transform T_min;
+    Transform T_min = null;
     float min_dist = Mathf.Infinity;
     public float cam_off = -10;
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-         transform.position = CloseToPlayer(Cam_pos).position;
+        Debug.Log(CloseToPlayer(Cam_pos));
     }
 
     Transform CloseToPlayer(Transform[] positions)
     {
         foreach (Transform t in positions)
         {
-            float dist = Vector3.Distance(t.position, player.transform.position);
+            //Vector3 directionToTarget = t.position - player.transform.position;
+
+            float dist = Vector3.Distance(t.position, player.position);
 
             if (dist < min_dist)
             {
-                T_min = t;
                 min_dist = dist;
+                T_min = t;
             }
         }
         return T_min;
