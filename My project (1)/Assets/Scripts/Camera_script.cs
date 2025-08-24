df0deb265438099b15b7c1b869c1c2b782307c18
EmmaTheCattt <Emmabuhl2004@gmail.com>
2025-08-24 20:01:11 +0200

    WHY NO WORK!!!!!

--- My project (1)/Assets/Scripts/Camera_script.cs
+++ My project (1)/Assets/Scripts/Camera_script.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class Camera_script : MonoBehaviour
 {
-    public GameObject[] Cam_pos;
+    public Transform[] Cam_pos;
     public GameObject player;
 
-    public Vector2 distance;
-    public Vector2 shortest_dis;
+    public Transform T_min;
+    float min_dist = Mathf.Infinity;
     public float cam_off = -10;
 
     // Start is called before the first frame update
@@ -20,18 +20,21 @@ public class Camera_script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        shortest_dis = new Vector2(player.transform.position.x - Cam_pos[0].transform.position.x, player.transform.position.y - Cam_pos[0].transform.position.y);
+         transform.position = CloseToPlayer(Cam_pos).position;
+    }
 
-        for (int i = 0; i < Cam_pos.Length; i++)
+    Transform CloseToPlayer(Transform[] positions)
+    {
+        foreach (Transform t in positions)
         {
-            distance = new Vector2(player.transform.position.x - Cam_pos[0].transform.position.x, player.transform.position.y - Cam_pos[0].transform.position.y);
+            float dist = Vector3.Distance(t.position, player.transform.position);
 
-            if (shortest_dis.magnitude > distance.magnitude)
+            if (dist < min_dist)
             {
-                shortest_dis = distance;
-                transform.position = new Vector3(Cam_pos[i].transform.position.x, Cam_pos[i].transform.position.y, cam_off);
+                T_min = t;
+                min_dist = dist;
             }
         }
+        return T_min;
     }
 }
