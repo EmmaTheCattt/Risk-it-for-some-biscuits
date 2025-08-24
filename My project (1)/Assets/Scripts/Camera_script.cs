4505c3aa5ed66c2898d8da07dd7841bae0d27745
EmmaTheCattt <Emmabuhl2004@gmail.com>
2025-08-24 20:58:18 +0200

    Working cam

--- My project (1)/Assets/Scripts/Camera_script.cs
+++ My project (1)/Assets/Scripts/Camera_script.cs
@@ -9,7 +9,7 @@ public class Camera_script : MonoBehaviour
     public Transform player;
 
     Transform T_min = null;
-    float min_dist = Mathf.Infinity;
+    public float min_dist = Mathf.Infinity;
     public float cam_off = -10;
 
     // Start is called before the first frame update
@@ -22,20 +22,25 @@ public class Camera_script : MonoBehaviour
     void Update()
     {
         Debug.Log(CloseToPlayer(Cam_pos));
+        transform.position = new Vector3(CloseToPlayer(Cam_pos).position.x, CloseToPlayer(Cam_pos).position.y, cam_off);
     }
 
     Transform CloseToPlayer(Transform[] positions)
     {
+        float min_dist = Mathf.Infinity;
+
         foreach (Transform t in positions)
         {
             //Vector3 directionToTarget = t.position - player.transform.position;
 
             float dist = Vector3.Distance(t.position, player.position);
+            Debug.Log(player.position);
+            Debug.Log(min_dist);
 
             if (dist < min_dist)
             {
-                min_dist = dist;
                 T_min = t;
+                min_dist = dist;
             }
         }
         return T_min;
